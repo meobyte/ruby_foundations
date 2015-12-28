aaab283883c10d66b825d2f0052e7f256de87284
Michelle Anderson <meobyte@gmail.com>
2015-12-28 04:31:27 +0000

    more TodoList methods

--- lesson_1/todo.rb
+++ lesson_1/todo.rb
@@ -114,6 +114,30 @@ class TodoList
 
     result
   end
+
+  def find_by_title(title)
+    select { |todo| todo.title == title }.first
+  end
+
+  def all_done
+    select { |todo| todo.done? }
+  end
+
+  def all_not_done
+    select { |todo| !todo.done? }
+  end
+
+  def mark_done(title)
+    find_by_title(title) && find_by_title(title).done!
+  end
+
+  def mark_all_done
+    each { |todo| todo.done! }
+  end
+
+  def mark_all_undone
+    each { |todo| todo.undone! }
+  end
 end
 
 todo1 = Todo.new("Buy milk")
@@ -127,6 +151,7 @@ list.add(todo3)
 
 todo1.done!
 
-results = list.select { |todo| todo.done? }
+puts list.all_done
 
-puts results.inspect
+find = list.find_by_title("Clean room")
+puts find
