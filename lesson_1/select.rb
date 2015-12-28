a3a32cec899b8d816c678f32f57d7c4384649c2d
Michelle Anderson <meobyte@gmail.com>
2015-12-28 03:39:58 +0000

    todo collection

--- lesson_1/select.rb
+++ lesson_1/select.rb
@@ -1,16 +1,15 @@
 def select(array)
   counter = 0
   result = []
-  
+
   while counter < array.size
     current_element = array[counter]
     result << current_element if yield(current_element)
     counter += 1
   end
-  
+
   result
 end
-
 
 array = [1, 2, 3, 4, 5]
 
---
+++ lesson_1/todo.rb
@@ -0,0 +1,114 @@
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def <<(todo)
+    raise TypeError, 'can only add Todo objects' unless todo.instance_of? Todo
+
+    @todos << todo
+  end
+  alias_method :add, :<<
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos.first
+  end
+
+  def last
+    @todos.last
+  end
+
+  def item_at(index)
+    @todos.fetch(index)
+  end
+
+  def mark_done_at(index)
+    item_at(index).done!
+  end
+
+  def mark_undone_at(index)
+    item_at(index).undone!
+  end
+
+  def done!
+    @todos.each_index do |index|
+      mark_done_at(index)
+    end
+  end
+
+  def done?
+    @todos.all?(&:done?)
+  end
+
+  def shift
+    @todos.shift
+  end
+
+  def pop
+    @todos.pop
+  end
+
+  def remove_at(index)
+    @todos.delete(item_at(index))
+  end
+
+  def to_s
+    text = "---- #{title} ----\n"
+    text << @todos.map(&:to_s).join("\n")
+    text
+  end
+
+  def to_a
+    @todos
+  end
+end
+
+todo1 = Todo.new("Buy milk")
+todo2 = Todo.new("Clean room")
+todo3 = Todo.new("Go to gym")
+list = TodoList.new("Today's Todos")
+
+list.add(todo1)
+list.add(todo2)
+list.add(todo3)
+
+puts list
+puts list.done?
+list.done!
+puts list.done?
