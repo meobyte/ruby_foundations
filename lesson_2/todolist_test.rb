4ed70ee085e391561edbf9ea170d6201133d6865
Michelle Anderson <meobyte@gmail.com>
2015-12-28 21:34:26 +0000

    setup test suit for todolist

---
+++ lesson_2/todolist_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/reporters'
+Minitest::Reporters.use!
+
+require_relative 'todolist'
+
+class TodoListTest < MiniTest::Test
+  def setup
+    @todo1 = Todo.new('Buy Milk')
+    @todo2 = Todo.new('Clean room')
+    @todo3 = Todo.new('Go to gym')
+    @todos = [@todo1, @todo2, @todo3]
+
+    @list = TodoList.new("Today's Todos")
+    @list.add(@todo1)
+    @list.add(@todo2)
+    @list.add(@todo3)
+  end
+
+
+end
