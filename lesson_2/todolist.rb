320f6c086f720934e87ac7f03d979f6d1fb1082f
Michelle Anderson <meobyte@gmail.com>
2015-12-28 22:25:15 +0000

    unit tests for todolist

--- lesson_2/todolist.rb
+++ lesson_2/todolist.rb
@@ -139,19 +139,3 @@ class TodoList
     each { |todo| todo.undone! }
   end
 end
-
-todo1 = Todo.new("Buy milk")
-todo2 = Todo.new("Clean room")
-todo3 = Todo.new("Go to gym")
-list = TodoList.new("Today's Todos")
-
-list.add(todo1)
-list.add(todo2)
-list.add(todo3)
-
-todo1.done!
-
-puts list.all_done
-
-find = list.find_by_title("Clean room")
-puts find
