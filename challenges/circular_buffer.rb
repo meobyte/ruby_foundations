0944fbefe564c4d5e8a14fd7471f4fba4dffb824
Michelle Anderson <meobyte@gmail.com>
2016-01-23 22:03:31 +0000

    refactor with yield block

--- challenges/circular_buffer.rb
+++ challenges/circular_buffer.rb
@@ -4,11 +4,13 @@ class CircularBuffer
 
   def initialize(buffer_size)
     @buffer_size = buffer_size
-    clear
+    @buffer = Array.new(buffer_size)
+    @head = 0
+    @tail = 0
   end
 
   def read
-    fail BufferEmptyException if @buffer.compact.empty?
+    fail BufferEmptyException if buffer.compact.empty?
     data = buffer[head]
     buffer[head] = nil
     advance_head
@@ -16,40 +18,44 @@ class CircularBuffer
   end
 
   def write(data)
-    return if data.nil?
-    fail BufferFullException if @buffer.all?
-    buffer[tail] = data
-    advance_tail
+    push(data) { fail BufferFullException }
   end
 
   def write!(data)
-    return if data.nil?
-    if @buffer.all?
+    push(data) do
       buffer[head] = data
       @tail = head
       advance_head
-    else
-      buffer[tail] = data
-      advance_tail
     end
   end
 
   def clear
-    @buffer = Array.new(buffer_size)
-    @head = 0
-    @tail = 0
+    buffer.fill(nil)
+  end
+
+  private
+
+  def push(data)
+    return if data.nil?
+
+    if buffer.all?
+      yield
+    end
+
+    buffer[tail] = data
+    advance_tail
   end
 
   def advance_head
     @head += 1
-    @head = 0 if @head == buffer_size
+    @head = 0 if head == buffer_size
   end
 
   def advance_tail
     @tail += 1
-    @tail = 0 if @tail == buffer_size
+    @tail = 0 if tail == buffer_size
   end
 
-  BufferEmptyException = Class.new(StandardError)
-  BufferFullException = Class.new(StandardError)
+  class BufferEmptyException < StandardError; end
+  class BufferFullException < StandardError; end
 end
